115226599879c4b00274b620d76d3fd608cc413d
Daniel Interiano <dannyboyint@gmail.com>
2022-03-30 12:22:35 -0400

    commit tree 2c701ae064bba7068e3eb57f4949941fe1536181

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+    validates :content, length:{minimum: 250}
+    validates :summary, length:{maximum: 250}
+    validates :title, presence: true
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+     validate :clickbait?
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top/i,
+    /Guess/i
+  ]
+    
+    
+      def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+
+          errors.add(:title, "must be clickbait")
+        end
+      end
 end
+
